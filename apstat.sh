b71305cbee6eea65db368bc065cd19f2d56416aa
Russ Thompson <vigornj@gmail.com>
2012-02-17 21:54:17 -0500

    new:  top remote connections.  fix:  debian compatability

--- apstat.sh
+++ apstat.sh
@@ -8,7 +8,26 @@ func_echo() {
 
 # Get parent pid.
 # Parent pid can also be obtained through /proc/pid/status
-PARENT_PID=`ps -eo ppid,cmd,user | grep 'apache' | grep -v "grep" | head -n 1 | awk '{print $1}'`
+if [ -z "$1" ] ; then
+
+	if [ -f /var/run/apache2.pid ] ; then
+		PARENT_PID=`cat /var/run/apache2.pid`
+		ARCH="apache2"
+	elif [ -f /var/run/httpd.pid ] ; then
+		PARENT_PID=`cat /var/run/httpd.pid`
+		ARCH="httpd"
+	elif [ -f /var/run/apache.pid ] ; then
+		PARENT_PID=`cat /var/run/apache.pid`
+		ARCH="apache"
+	else
+		func_echo "Error:  unable to find apache pid, is it running? Alternatively you can provide the pid at runtime (i.e 
+./apstat.sh pid)."
+	fi
+
+else
+	PARENT_PID="$1"
+	ARCH="$1"
+fi
 
 # Process priority (Parent)
 PID_PRIORITY=`cat /proc/$PARENT_PID/stat | awk '{print $19}'`
@@ -17,44 +36,45 @@ PID_PRIORITY=`cat /proc/$PARENT_PID/stat | awk '{print $19}'`
 START_TIME=`ls -ld /proc/$PARENT_PID | awk '{print $6,$7,$8}' | cut -d"/" -f1`
 
 # Number of open files
-OPEN_FILES=`lsof | grep httpd | wc -l`
+OPEN_FILES=`lsof | grep $ARCH | wc -l`
 
 # Generate CPU utilization
 
 SUM="0"
-for i in `ps -eo %cpu,pid,cmd|sort -k2 -r | grep 'httpd' | grep -v "grep" | awk '{print $1}'` ; do
+for i in `ps -eo %cpu,pid,cmd|sort -k2 -r | grep '$ARCH' | grep -v "grep" | awk '{print $1}'` ; do
 	SUM="$(echo $SUM + $i | bc)"
 done
 
 # Generate memory utilization.
-MEMORY_USAGE=`ps aux | grep httpd | grep -v "grep" | awk '{ s += $6 } END { print s/1024, "Mb"}'`
+MEMORY_USAGE=`ps aux | grep $ARCH | grep -v "grep" | awk '{ s += $6 } END { print s/1024, "Mb"}'`
 
 # Threads (Childs) - Total including parent.
-NO_THREADS=`ps -ef | grep httpd | grep -v "grep" | wc -l`
+NO_THREADS=`ps -ef | grep $ARCH | grep -v "grep" | wc -l`
 
 # Connections total
-CONN_TOTAL=`netstat -ap | grep httpd | wc -l`
+CONN_TOTAL=`netstat -ap | grep $ARCH | wc -l`
 
 # Connections in SYN
-SYN_STATE=`netstat -ap | grep httpd | grep 'SYN' | wc -l`
+SYN_STATE=`netstat -ap | grep $ARCH | grep 'SYN' | wc -l`
 
 # Connections in TimeWAIT
-WAIT_STATE=`netstat -ap | grep httpd | grep 'TIME_WAIT' | wc -l`
+WAIT_STATE=`netstat -ap | grep $ARCH | grep 'TIME_WAIT' | wc -l`
 
 # Connections in LISTEN
-LISTEN_STATE=`netstat -ap | grep httpd | grep 'LISTEN' | wc -l`
+LISTEN_STATE=`netstat -ap | grep $ARCH | grep 'LISTEN' | wc -l`
 
 # Connections in ESTABLISHED/CONNECTED
-ESTB_STATE=`netstat -ap | grep httpd | grep -e 'ESTABLISHED' -e 'CONNECTED' | wc -l`
+ESTB_STATE=`netstat -ap | grep $ARCH | grep -e 'ESTABLISHED' -e 'CONNECTED' | wc -l`
 
 # Highest connection count per IP
-CONN_COUNT_IP=`netstat -tp | grep http | awk '{print $4}' | awk -F':' '{print $4}' |  grep ^[0-9] | uniq -c | sort -rn | head -n 10`
+CONN_COUNT_IP=`netstat -tp | grep $ARCH | awk '{print $4}' | awk -F':' '{print $4}' |  grep ^[0-9] | uniq -c | sort -rn | head -n 
+10`
 
 
 # Generate IO statistics.
 READ_SUM="0" ; WRITE_SUM="0" ; READ_BSUM="0" ; WRITE_BSUM="0"
 
-for i in `pidof httpd | awk -F " " '{print}'` ; do 
+for i in `pidof $ARCH | awk -F " " '{print}'` ; do 
 	READ_COUNT=`cat /proc/$i/io | grep 'rchar' | awk '{print $2}'`
 	WRITE_COUNT=`cat /proc/$i/io | grep 'wchar' | awk '{print $2}'`
 	READ_BCOUNT=`cat /proc/$i/io | grep 'read_bytes' | awk '{print $2}'`
@@ -73,7 +93,7 @@ func_echo "General Statistics\t"
 	echo "Open file count:  $OPEN_FILES"
 
 func_echo "Connectivity\t"
-	echo "Total Connections:  $CONN_TOTAL"
+	echo "Total Active Sockets:  $CONN_TOTAL"
 	echo "SYN Connections:  $SYN_STATE"
 	echo "Wait Connections:  $WAIT_STATE"
 	echo "Etsablished Connections:  $ESTB_STATE"
@@ -89,4 +109,10 @@ func_echo "IO Statistics\t"
 	echo "Write Count:  $WRITE_SUM"
 	echo "Read Bytes:  $READ_BSUM"
 	echo "Write Bytes:  $WRITE_BSUM"
+func_echo "Remote connections (top 10): "
+	if [ -z "$CONN_COUNT_IP" ] ; then
+		echo "none found"
+	else
+		echo "$CONN_COUNT_IP"
+	fi
 
